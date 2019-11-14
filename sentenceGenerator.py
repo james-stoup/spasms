a2487eb66fcffcc8506314caa649513dcae63bbb
kornonfire <thomasklock14@gmail.com>
2019-11-14 16:00:50 -0500

    Added verbs to sentence generation.

--- sentenceGenerator.py
+++ sentenceGenerator.py
@@ -17,6 +17,10 @@ def generateSentence(cur, noun, sentim):
 		cur.execute("SELECT word FROM words WHERE (sentiment > 0) and (type = 'adverb')")
 		numOfAdverbs = cur.rowcount
 		adverbs = cur.fetchall()
+
+		cur.execute("SELECT word FROM words WHERE (sentiment > 0) and (type = 'verb')")
+		numOfVerbs = cur.rowcount
+		verbs = cur.fetchall()
 	elif sentim == "neg":
 		cur.execute("SELECT word FROM words WHERE (sentiment < 0) and (type = 'adjective')")
 		numOfAdjectives = cur.rowcount
@@ -25,6 +29,10 @@ def generateSentence(cur, noun, sentim):
 		cur.execute("SELECT word FROM words WHERE (sentiment < 0) and (type = 'adverb')")
 		numOfAdverbs = cur.rowcount
 		adverbs = cur.fetchall()
+
+		cur.execute("SELECT word FROM words WHERE (sentiment < 0) and (type = 'Verb')")
+		numOfVerbs = cur.rowcount
+		verbs = cur.fetchall()
 
 	fragSplit = frag.split(" ")
 	for x in range(len(fragSplit)):
@@ -36,6 +44,9 @@ def generateSentence(cur, noun, sentim):
 		elif fragSplit[x] == '<adverb':
 			randAdvNum = random.randrange(0, numOfAdverbs, 1)
 			fragSplit[x] = adverbs[randAdvNum][0]
+		elif fragSplit[x] == '<verb>':
+			randVerbNum = random.randrange(0, numOfVerbs, 1)
+			fragSplit[x] = verbs[randVerbNum][0]
 
 
 	fragJoin = " ".join(fragSplit)
