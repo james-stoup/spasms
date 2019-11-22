f091a0581d3e460eb9d4219fbf92ce9ec571e23d
sharonmjsoe <sharonmjose101@gmail.com>
2019-11-22 13:30:32 -0500

    removed tweets.json file

--- exportJson.py
+++ exportJson.py
@@ -1,27 +1,6 @@
 import json;
 
 def exportTweets(cursor, topic, fileName):
-	data = [];
-	data.append({
-		"created_at": "2019-11-20 03:00:00",
-		"id": 123456789,
-		"id_str": "123456789",
-		"text": "My name is Bob",
-		"user": {
-			"id": 1,
-			"name": "Bob"
-		}
-	});
-	data.append({
-		"created_at": "2019-11-20 03:30:00",
-		"id": 549861901,
-		"id_str": "111111111",
-		"text": "My name is Jane",
-		"user": {
-			"id":2,
-			"name":"Jane"
-		}
-	});
 	cursor.execute("SELECT * FROM Twitter_posts WHERE topic=%s",(topic,));
 	twitterData = [];
 	tweets = cursor.fetchall();
@@ -56,4 +35,3 @@ def exportTweets(cursor, topic, fileName):
 		});
 	outputFile = open(fileName,"w");
 	json.dump(twitterData,outputFile);
-
