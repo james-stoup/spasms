32d61a690ab8ff71347efe58d648055df773da13
James Stoup <jrstoup@gmail.com>
2019-12-11 17:07:04 -0500

    Initial feed back on the code
    Some minor reworks to make things cleaner. Some cleaning up
    of duplication. Added some notes.

--- generateTwitterUser.py
+++ generateTwitterUser.py
@@ -1,8 +1,9 @@
 import psycopg2
 import random
+from random import seed
+from random import randint
 from genDescription import generateDescription
 from genTimestamp import generateTime
-
 
 # def connect():
 # 	conn = psycopg2.connect(database="spasms", user="postgres", password="1514729", host="/var/run/postgresql", port="5432")
@@ -14,67 +15,98 @@ from genTimestamp import generateTime
 #following line for picking between male and female name
 #cur.execute("SELECT COUNT(*) FROM names")
 
+
+
+def grab_data(cur, statement_1, statement_2):
+	
+	cur.execute(statement_1)
+	firstRow = cur.fetchone()[0]
+	randVal = random.randrange(0, firstRow, 1)
+
+	cur.execute(statement_2)
+	rows = cur.fetchall()
+	result = rows[randVal][0]
+
+	return result
+
+
+
+
+
 #cur is a connection to the database
 #groupName is the group of the individuals created
 #gender_type takes in a argument of 'first_m' or 'first_f' and is the gender of the individuals crated
-def createTwitterUser(cur,groupName,gender_type):
+def createTwitterUser(cur, groupName, gender_type):
 
+	gender = 'first_f'
 	if gender_type == 'm':
 		gender = 'first_m'
-	else:
-		gender = 'first_f'
 
-	cur.execute("SELECT count(*) from names where name_type='"+gender+"'")
-	firstNamesNum = cur.fetchone()[0]
+	# first name
+	str_1 = "SELECT count(*) from names where name_type='"+gender+"'"
+	str_2 = "SELECT name, name_type from names where name_type='"+gender+"'"
+	firstName = grab_data(cur, str_1, str_2)
+	
+	# last name
+	str_1 = "Select count(*) from names where Name_type='last'"
+	str_2 = "SELECT name from names where Name_type='last'"
+	lastName = grab_data(cur, str_1, str_2)
 
-	randFirstName = random.randrange(0,firstNamesNum,1)
+	# location
+	str_1 = "SELECT COUNT(*) from Locations where Country='United States'"
+	str_2 = "SELECT city from Locations where Country='United States'"
+	randomLocation = grab_data(cur, str_1, str_2)
 
-	cur.execute("SELECT name,name_type from names where name_type='"+gender+"'")
-	firstNames = cur.fetchall()
-	firstName = firstNames[randFirstName][0]
-	firstLetter = firstName[0]
-
-	cur.execute("Select count(*) from names where Name_type='last'")
-	lastNamesNum = cur.fetchone()[0]
-
-	randLastName = random.randrange(0,lastNamesNum,1)
-
-	cur.execute("SELECT name from names where Name_type='last'")
-	lastNames = cur.fetchall()
-
-	lastName = lastNames[randLastName][0]
-
-	cur.execute("SELECT COUNT(*) from Locations where Country='United States'")
-	locationsNum = cur.fetchone()[0]
-#print(locationsNum)
-
-	randLocation = random.randrange(0,locationsNum,1)
-
-
-	cur.execute("SELECT city from Locations where Country='United States'")
-	locations = cur.fetchall()
-	randomLocation = locations[randLocation][0]
-
-
-
+	# twitter real name
 	twitter_name = firstName +" "+ lastName
+
+	# twitter screen name
+	firstLetter = firstName[0]
 	lastName = lastName.lower()
 	twitter_screen_name = firstLetter + lastName
-#would like to make this a choice between 3 numbers such as 10, 10000, 100000
+
+
+	### NOTES ###
+	###
+	### Maybe consider adding an option to randomize the twitter handles
+	### so that everyone isn't the same "first initial last name" pattern?
+	### Here is an example of what I mean.
+	###
+	### !!! Oh, you need to alter the table so the screen_name is more than 15 chars
+	### like so:
+	### alter table twitter_users alter COLUMN screen_name type character varying(100);
+	###
+	
+	colors = ["blue", "black", "green", "yellow", "red", "white", "teal", "orange", "lilac", "neon", "clear"]
+	adjs = ["avenging", "strident", "lucky", "sexy", "funky", "dancing", "weaving", "somnombulating"]
+	critters = ["angel", "dog", "whale", "shield", "sword", "knight", "pony", "snake", "salesman", "cat"]
+	name_modifiers = [colors, adjs]
+	
+	value = randint(0, 100)
+	if value > 60:  # 40% of the time make a random user name
+		coin = randint(0, 1)
+		modifier_list = name_modifiers[coin] # pick a color or an adjective
+		first = random.choice(modifier_list) # pick a value from the list
+		second = random.choice(critters)     # pick a noun
+		third = randint(0, 100)              # and for good measure, pick a number
+		twitter_screen_name = "%s_%s%s" % (first, second, third)
+	
+	
+	#would like to make this a choice between 3 numbers such as 10,  10000,  100000
 	randomVal = 10000
 	userCount = []
 
 	for i in range(4):
-		randNum = random.randrange(0,randomVal,1)
+		randNum = random.randrange(0, randomVal, 1)
 		userCount.append(randNum)
 	
 	followerCount = userCount[0]
 	#friendsCount = userCount[1]
 	favouritesCount = userCount[2]
 	statusesCount =userCount[3]
-#only goes up to 7 characters
+	#only goes up to 7 characters
 	randomIdVal = 100000000
-	randId= random.randrange(0,randomIdVal,1)
+	randId= random.randrange(0, randomIdVal, 1)
 	randIdStr = str(randId)
 
 	description = generateDescription(10)
@@ -87,34 +119,46 @@ def createTwitterUser(cur,groupName,gender_type):
 	# else:
 	# 	gender = 'f'
 
-	tupleTwitterUser = (randId,randIdStr,twitter_name,twitter_screen_name,randomLocation,generateTime(start,end),followerCount,favouritesCount,statusesCount,description,gender_type,groupName) 
+	tupleTwitterUser = (
+		randId, 
+		randIdStr, 
+		twitter_name, 
+		twitter_screen_name, 
+		randomLocation, 
+		generateTime(start, end), 
+		followerCount, 
+		favouritesCount, 
+		statusesCount, 
+		description, 
+		gender_type, 
+		groupName)
+	
 	return tupleTwitterUser
 
 
 
-def insertTwitterUsers(cur,groupName,numUsers,gender_type):
+def insertTwitterUsers(cur, groupName, numUsers, gender_type):
 	#add exception handling here	
 #	howManyUsers = int(raw_input('Enter how many users you want: '))
 #	outputFileName = raw_input('Enter output file name: ')
 	
-#	outputFile = open(outputFileName,'w')	
+#	outputFile = open(outputFileName, 'w')	
 
 	#conn = connect()
 	#cur = conn.cursor()
 
 
 	for i in range(numUsers):
-	
-		twitterUser = createTwitterUser(cur,groupName,gender_type)
+		twitterUser = createTwitterUser(cur, groupName, gender_type)
 								
-		cur.execute("INSERT INTO twitter_users (id,id_str,name,screen_name,location,created_at,followers,favourites,statuses,description,gender,group_name) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",twitterUser)
+		cur.execute("INSERT INTO twitter_users (id, id_str, name, screen_name, location, created_at, followers, favourites, statuses, description, gender, group_name) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", twitterUser)
 	
 	#conn.commit()				
 		
 
 # conn = connect()
 # cur = conn.cursor()
-#createTwitterUser(cur,"red","first_f")
-# insertTwitterUsers("American",3,"first_f")
+#createTwitterUser(cur, "red", "first_f")
+# insertTwitterUsers("American", 3, "first_f")
 # conn.close()
 # print("Database connection closed")
--- spasmsMain.py
+++ spasmsMain.py
@@ -1,41 +1,83 @@
+#!/usr/bin/env python
+
+
 import psycopg2
 import genTweet
 import generateTwitterUser
 import exportJson
 import math
+import sys
 
+
+def connect_to_db(db_name):
+	"""Try to connect to the DB, return connection if successful"""
+	conn = psycopg2.connect(
+		database=db_name,
+		user="postgres",
+		password="wowarcraft12",
+		host="/var/run/postgresql",
+		port="5432",
+	)
+
+	return conn
+
+
+def get_input(prompt):
+	"""Prompt the user for input until they enter valid data"""
+	while True:
+		user_input = input("%38s : " % prompt)
+		if not user_input.strip():
+			print("Please enter valid input")
+		else:
+			return user_input
 
 def main():
-	conn = psycopg2.connect(database="spasms", user="postgres", password="wowarcraft12", host="/var/run/postgresql", port="5432")
+
+	conn = connect_to_db("spasms")
 	cur = conn.cursor()
 
-	groupName = raw_input("Group name: ")
-	topicName = raw_input("Topic name: ")
-	numberOfUsers = raw_input ("Number of users: ")
-	genderPercentage = raw_input("Percent of users to be female: ")
-	typeOfPosts = raw_input("Twitter or Facebook posts: ")
-	numberOfPosts = raw_input("Number of posts: ")
-	sentimentOfPosts = raw_input("Positive (pos) or Negative (neg) posts: ")
-	nounOfPosts = raw_input("Please input noun relating to topic for tweets: ")
-	nameOfJsonFile = raw_input("Name for json file output: ")
+	# get input
+	groupName = get_input("Group name")
+	topicName = get_input("Topic name")
+	numberOfUsers = get_input("Number of users")
+	genderPercentage = get_input("Percent of users to be female")
+	typeOfPosts = get_input("Select Post Type (Twitter/Facebook)")
+	numberOfPosts = get_input("Number of posts")
+	sentimentOfPosts = get_input("Select Attitude (pos/neg")
+	nounOfPosts = get_input("Enter noun relating to topic")
+	nameOfJsonFile = get_input("Name for json file output")
 
-	genderPercentage = int(genderPercentage)/100
-	numberOfFemales = int(int(numberOfUsers)*genderPercentage)
+	genderPercentage = int(genderPercentage) / 100
+	numberOfFemales = int(int(numberOfUsers) * genderPercentage)
 	numberOfMales = int(numberOfUsers) - numberOfFemales
 
 	if typeOfPosts.lower() == "twitter":
 		if numberOfFemales > 0:
 			generateTwitterUser.insertTwitterUsers(cur, groupName, numberOfFemales, "f")
+
 		if numberOfMales > 0:
 			generateTwitterUser.insertTwitterUsers(cur, groupName, numberOfMales, "m")
-		genTweet.genTweet(cur, groupName, nounOfPosts, sentimentOfPosts, topicName, int(numberOfPosts))
+				
+		genTweet.genTweet(
+			cur,
+			groupName,
+			nounOfPosts,
+			sentimentOfPosts,
+			topicName,
+			int(numberOfPosts),
+		)
+			
 		conn.commit()
-	        exportJson.exportTweets(cur, topicName, nameOfJsonFile)
+		exportJson.exportTweets(cur, topicName, nameOfJsonFile)
 		print("Twitter posts have been generated and file successfully exported")
+			
 	elif typeOfPosts.lower() == "facebook":
 		print("Feature under development")
+	else:
+		sys.exit("Invalid Feature Selected. Goodbye.")
 
 	conn.close()
 
-if __name__ == '__main__':
-	main()
+
+if __name__ == "__main__":
+    main()
