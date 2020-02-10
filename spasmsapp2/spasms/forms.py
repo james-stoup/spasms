e59881b7e5a46261e078b444e06cee1f432a3d2b
sharonmjsoe <sharonmjose101@gmail.com>
2020-02-10 11:40:06 -0500

    Adde more input fields to the form

--- spasmsapp2/spasms/forms.py
+++ spasmsapp2/spasms/forms.py
@@ -3,5 +3,11 @@ from django import forms
 class NameForm(forms.Form):
 	group_name = forms.CharField(label='Group name', max_length=100)
 	topic_name = forms.CharField(label='Topic name', max_length=200)
-	
-
+	num_users = forms.IntegerField()
+	percent_female = forms.IntegerField()
+	post_options = [('1','Twitter'),('2','Facebook')]
+	twitter_or_facebook = forms.ChoiceField(choices=post_options,label='Type of Posts')
+	num_posts = forms.IntegerField()
+	sentiments = [(1,'positive'),(2,'negative')]
+	sentiment = forms.ChoiceField(choices=sentiments,label='Sentiment')
+	#topic_noun = forms.CharField(lebel='Noun relating to topic', max_length=100)
