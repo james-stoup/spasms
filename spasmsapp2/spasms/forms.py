7af2c68d30db9236f75e4ea1f7c94eeabaeee7c1
kornonfire <thomasklock14@gmail.com>
2020-02-12 10:59:11 -0500

    Set initial, min, and max values for users, posts, and percentage fields and changed the tuples for sentiment and post type to return expected values.

--- spasmsapp2/spasms/forms.py
+++ spasmsapp2/spasms/forms.py
@@ -3,11 +3,11 @@ from django import forms
 class NameForm(forms.Form):
 	group_name = forms.CharField(label='Group name', max_length=100)
 	topic_name = forms.CharField(label='Topic name', max_length=200)
-	num_users = forms.IntegerField()
-	percent_female = forms.IntegerField()
-	post_options = [('1','Twitter'),('2','Facebook')]
+	num_users = forms.IntegerField(initial=1, min_value=1)
+	percent_female = forms.IntegerField(initial=50, min_value=0, max_value=100)
+	post_options = [('twitter','Twitter'),('facebook','Facebook')]
 	twitter_or_facebook = forms.ChoiceField(choices=post_options,label='Type of Posts')
-	num_posts = forms.IntegerField()
-	sentiments = [(1,'positive'),(2,'negative')]
+	num_posts = forms.IntegerField(initial=1, min_value=1)
+	sentiments = [('pos','positive'),('neg','negative')]
 	sentiment = forms.ChoiceField(choices=sentiments,label='Sentiment')
 	#topic_noun = forms.CharField(lebel='Noun relating to topic', max_length=100)
