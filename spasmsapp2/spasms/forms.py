f5edbff3838e51d0b450117483488a28fd30e455
kornonfire <thomasklock14@gmail.com>
2020-02-21 11:51:59 -0500

    Changed input over to models and used ModelForm to render the inputs now.

--- spasmsapp2/spasms/forms.py
+++ spasmsapp2/spasms/forms.py
@@ -1,14 +1,21 @@
 from django import forms
+from .models import InputModel
+from django.forms import ModelForm
 
-class NameForm(forms.Form):
-	group_name = forms.CharField(label='Group name', max_length=100)
-	topic_name = forms.CharField(label='Topic name', max_length=200)
-	num_users = forms.IntegerField(initial=1, min_value=1)
-	percent_female = forms.IntegerField(initial=50, min_value=0, max_value=100)
-	post_options = [('twitter','Twitter'),('facebook','Facebook')]
-	twitter_or_facebook = forms.ChoiceField(choices=post_options,label='Type of Posts')
-	num_posts = forms.IntegerField(initial=1, min_value=1)
-	sentiments = [('pos','positive'),('neg','negative')]
-	sentiment = forms.ChoiceField(choices=sentiments,label='Sentiment')
-	topic_noun = forms.CharField(label='Noun relating to topic', max_length=100)
-	json_output = forms.CharField(label='Name for json file output', max_length=100)
+class InputModelForm(ModelForm):
+	class Meta:
+		model = InputModel
+		fields = ['group_name', 'topic_name', 'num_users', 'percent_female', 'twitter_or_facebook', 'num_posts', 'sentiment', 'topic_noun', 'json_output']
+
+# class NameForm(forms.Form):
+# 	group_name = forms.CharField(label='Group name', max_length=100)
+# 	topic_name = forms.CharField(label='Topic name', max_length=200)
+# 	num_users = forms.IntegerField(initial=1, min_value=1)
+# 	percent_female = forms.IntegerField(initial=50, min_value=0, max_value=100)
+# 	post_options = [('twitter','Twitter'),('facebook','Facebook')]
+# 	twitter_or_facebook = forms.ChoiceField(choices=post_options,label='Type of Posts')
+# 	num_posts = forms.IntegerField(initial=1, min_value=1)
+# 	sentiments = [('pos','positive'),('neg','negative')]
+# 	sentiment = forms.ChoiceField(choices=sentiments,label='Sentiment')
+# 	topic_noun = forms.CharField(label='Noun relating to topic', max_length=100)
+# 	json_output = forms.CharField(label='Name for json file output', max_length=100)
--- spasmsapp2/spasms/views.py
+++ spasmsapp2/spasms/views.py
@@ -3,7 +3,7 @@ sys.path.append(os.path.abspath(os.path.join('..','src')))
 
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
-from .forms import NameForm
+from .forms import InputModelForm
 
 from spasmsMain import spasms_main
 
@@ -17,7 +17,7 @@ def get_name(request):
 	# if this is a POST request we need to process the form data
 	if request.method == 'POST':
 		# create a form instance and populate it with data from the request:
-		form = NameForm(request.POST)
+		form = InputModelForm(request.POST)
 		# check whether it's valid:
 		if form.is_valid():
 			# process the data in form.cleaned_data as required
@@ -27,5 +27,5 @@ def get_name(request):
 			return HttpResponseRedirect('/thanks/')
 	# if a GET (or any other method) we'll create a blank form
 	else:
-		form = NameForm()
+		form = InputModelForm()
 	return render(request, 'name.html', {'form': form})
