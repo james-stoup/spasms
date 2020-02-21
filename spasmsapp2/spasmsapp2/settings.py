120a12de6a674f60c5283f148d110399d99ea9e7
kornonfire <thomasklock14@gmail.com>
2020-02-21 12:50:25 -0500

    Change model IntegerField to PositiveIntegerField.

--- spasmsapp2/spasmsapp2/settings.py
+++ spasmsapp2/spasmsapp2/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    	'django.contrib.admin',
-    	'django.contrib.auth',
-    	'django.contrib.contenttypes',
-    	'django.contrib.sessions',
-    	'django.contrib.messages',
-    	'django.contrib.staticfiles',
-	'spasms.apps.SpasmsConfig'
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'spasms.apps.SpasmsConfig'
 ]
 
 MIDDLEWARE = [
@@ -79,7 +79,7 @@ DATABASES = {
 	'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'spasms',
         'USER': 'postgres',
-        'PASSWORD': 'password',
+        'PASSWORD': '',
         'HOST': '/var/run/postgresql',
         'PORT': '5432',
     }
